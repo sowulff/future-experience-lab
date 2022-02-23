e10d36361e0a0384ab1e9e0b3d290872d53f5890
sowulff <sowu99@gmail.com>
2022-02-23 18:32:25 +0100

    script for eye movement

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ const navItems = document.querySelector(".menu-slide");
 hamburger.addEventListener("click", () => {
   navItems.classList.toggle("open");
 });
+
+// CHANGE POSITION OF EYE
+var loc = window.location.pathname;
+console.log(loc);
+
+var navHome = document.querySelector(".nav-item-home");
+var navOngoing = document.querySelector(".nav-item-ongoing");
+var navUpcoming = document.querySelector(".nav-item-upcoming");
+var navContact = document.querySelector(".nav-item-contact");
+
+if (loc.indexOf("ongoing") > -1) {
+  console.log("ongoing");
+}
