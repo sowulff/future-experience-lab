1bb6d01850afc10ea8bc719ea4cf64e50ca89ad1
amandahulten <amahul1029@skola.goteborg.se>
2022-02-25 18:17:45 +0100

    added examplepicture and buttons
    tried making eyes roll, didn't work

--- app.js
+++ app.js
@@ -16,3 +16,20 @@ var navContact = document.querySelector(".nav-item-contact");
 if (loc.indexOf("ongoing") > -1) {
   console.log("ongoing");
 }
+
+// GET EYES TO ROLL
+
+$(".move-area").mousemove(function (event) {
+  var eye = $(".eye");
+  console.log("eye", eye);
+  var x = eye.offset().left + eye.width() / 2;
+  var y = eye.offset().top + eye.height() / 2;
+  var rad = Math.atan2(event.pageX - x, event.pageY - y);
+  var rot = rad * (180 / Math.PI) * -1 + 180;
+  eye.css({
+    "-webkit-transform": "rotate(" + rot + "deg)",
+    "-moz-transform": "rotate(" + rot + "deg)",
+    "-ms-transform": "rotate(" + rot + "deg)",
+    transform: "rotate(" + rot + "deg)",
+  });
+});
